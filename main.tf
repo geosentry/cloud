f371c73a76a4ade071c10320352d6b234532093a
Manish Meganathan <meganathan.manish@gmail.com>
2021-07-07 11:44:27 +0530

    Add a GCP project resource

--- main.tf
+++ main.tf
@@ -1,3 +1,4 @@
+# Setup Terraform Providers
 terraform {
   required_providers {
     google = {
@@ -7,18 +8,28 @@ terraform {
   }
 }
 
+# Setup Variable for Project ID
 variable "project" {
   type        = string
   description = "Google Cloud Platform Project ID"
 }
 
+# Setup Variable for Region
 variable "region" {
   type    = string
   default = "asia-south1"
   description = "Google Cloud Platform Deployment Region"
 }
 
+# Setup GCP Provider
 provider "google" {
-  region = "asia-south1"
+  region = var.region
   project = var.project
+}
+
+# Setup GCP Project
+# terraform import google_project.geosentry-project <projectID>
+resource "google_project" "geosentry-project" {
+  name       = "GeoSentry"
+  project_id = var.project
 }
