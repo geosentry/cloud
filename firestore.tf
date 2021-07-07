a264d17a91b68990bce73daf3fdb61c9f8fdf2ca
Manish Meganathan <meganathan.manish@gmail.com>
2021-07-07 11:46:17 +0530

    Add app engine resource and firestore database

--- firestore.tf
+++ firestore.tf
@@ -4,4 +4,12 @@ resource "google_project_service" "firestore-api" {
   disable_on_destroy = false
 }
 
+# Create App Engine Instance and Firestore Database
+# terraform import google_app_engine_application.app <projectID>
+resource "google_app_engine_application" "app" {
+  project     = var.project
+  location_id = "asia-south1"
+  database_type = "CLOUD_FIRESTORE"
+}
+
 # TODO: Create Firestore Index and Collection Groups
