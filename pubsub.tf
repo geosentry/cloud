7d480230d6c6e3533bedd948d967045c4bcf8f73
Manish Meganathan <meganathan.manish@gmail.com>
2021-07-07 11:44:43 +0530

    Add import statement refs for pubsub topics

--- pubsub.tf
+++ pubsub.tf
@@ -5,16 +5,19 @@ resource "google_project_service" "pubsub-api" {
 }
 
 # Create for PubSub Topic for taskbuilds
+# terraform import google_pubsub_topic.taskbuilds taskbuilds
 resource "google_pubsub_topic" "taskbuilds" {
   name = "taskbuilds"
 }
 
 # Create for PubSub Topic for cleanups
+# terraform import google_pubsub_topic.cleanups cleanups
 resource "google_pubsub_topic" "cleanups" {
   name = "cleanups"
 }
 
 # Create for PubSub Topic for repeats
+# terraform import google_pubsub_topic.repeats repeats
 resource "google_pubsub_topic" "repeats" {
   name = "repeats"
 }
