839ebd091a0cec91a0f5c7515d136513aa9329e9
Manish Meganathan <meganathan.manish@gmail.com>
2021-07-10 21:38:17 +0530

    Add docs and generic service API resources
    - Moved the cloudbuild and earthengine api resources into this file
    - Added some extra docs

--- main.tf
+++ main.tf
@@ -8,20 +8,20 @@ terraform {
   }
 }
 
-# Setup Variable for Project ID
+# Setup Variable for GCP Project ID
 variable "project" {
   type        = string
   description = "Google Cloud Platform Project ID"
 }
 
-# Setup Variable for Region
+# Setup Variable for GCP Deployment Region
 variable "region" {
   type    = string
   default = "asia-south1"
   description = "Google Cloud Platform Deployment Region"
 }
 
-# Setup Provide for google
+# Setup Provider for google
 provider "google" {
   region = var.region
   project = var.project
@@ -38,4 +38,16 @@ provider "google-beta" {
 resource "google_project" "geosentry-project" {
   name       = "GeoSentry"
   project_id = var.project
+}
+
+# Enable Google Earth Engine API
+resource "google_project_service" "earthengine-api" {
+  service = "earthengine.googleapis.com"
+  disable_on_destroy = false
+}
+
+# Enable Cloud Build API
+resource "google_project_service" "cloudbuild-api" {
+  service = "cloudbuild.googleapis.com"
+  disable_on_destroy = false
 }
